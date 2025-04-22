6c915c85d262eab69ba8f0f73d9e92b7c5cccb1d
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-04-21 18:34:02 -0700

    ajustando ocelot

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Build
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /src
+
+COPY ../Gateway.Core.sln .
+COPY . ./Gateway.Core
+
+WORKDIR /src/Gateway.Core
+RUN dotnet restore
+RUN dotnet publish -c Release -o /app/publish
+
+# Runtime
+FROM mcr.microsoft.com/dotnet/aspnet:9.0
+WORKDIR /app
+COPY --from=build /app/publish .
+EXPOSE 80
+ENTRYPOINT ["dotnet", "Gateway.Core.dll"]
--- Gateway.Core/Program.cs
+++ Gateway.Core/Program.cs
@@ -3,13 +3,15 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"ocelot.{Environment.GetEnvironmentVariable("OCEL0T_ENV") ?? "local"}.json")
+    .AddEnvironmentVariables();
 
 builder.Services.AddOcelot();
 
 var app = builder.Build();
-
-
 await app.UseOcelot();
 
 app.Run();

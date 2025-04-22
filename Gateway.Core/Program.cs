b8edea1a59a90e31c9602ff12aef88ad72acd8f6
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-04-21 18:17:31 -0700

    add gateway

--- Gateway.Core/Program.cs
+++ Gateway.Core/Program.cs
@@ -1,23 +1,15 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-internal class Program
-{
-    private static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-        builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-        builder.Logging.ClearProviders();
-        builder.Logging.AddConsole();
-        builder.Logging.SetMinimumLevel(LogLevel.Debug);
-        builder.Services.AddOcelot();
+builder.Services.AddOcelot();
 
-        var app = builder.Build();
+var app = builder.Build();
 
-        await app.UseOcelot();
 
-        app.Run();
-    }
-}
+await app.UseOcelot();
+
+app.Run();

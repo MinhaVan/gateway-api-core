8a2c93a2ce99374b2780284caa12072bfa5c9b37
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-04-22 09:04:51 -0700

    Removendo Ocelot e utilizando Yarp

--- Gateway.Core/Program.cs
+++ Gateway.Core/Program.cs
@@ -1,21 +1,30 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+using Yarp.ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
-var env = Environment.GetEnvironmentVariable("OCEL0T_ENV") ?? "local";
+var env = Environment.GetEnvironmentVariable("ENV") ?? "local";
 
 Console.WriteLine($"Environment: {env}");
 Console.WriteLine($"Base Path: {Directory.GetCurrentDirectory()}");
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", true, true)
-    .AddJsonFile($"ocelot.{env}.json")
+    .AddJsonFile($"appsettings.{env}.json", false, true)
     .AddEnvironmentVariables();
 
-builder.Services.AddOcelot();
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
-await app.UseOcelot();
+
+app.UseWebSockets();
+
+app.Use(async (context, next) =>
+{
+    var traceId = context.TraceIdentifier;
+    Console.WriteLine($"[TRACE] {traceId} {context.Request.Method} {context.Request.Path}");
+    await next();
+});
+
+app.MapReverseProxy();
 
 app.Run();

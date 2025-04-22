6d31ce1f5d36ce2279ca99be7d0b80bf39c88c50
Walterli Valadares Jr. <walterli.valadares@outlook.com>
2025-04-21 18:52:35 -0700

    capturando configs por ambiente

--- Gateway.Core/Program.cs
+++ Gateway.Core/Program.cs
@@ -2,11 +2,15 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+var env = Environment.GetEnvironmentVariable("OCEL0T_ENV") ?? "local";
+
+Console.WriteLine($"Environment: {env}");
+Console.WriteLine($"Base Path: {Directory.GetCurrentDirectory()}");
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", true, true)
-    .AddJsonFile($"ocelot.{Environment.GetEnvironmentVariable("OCEL0T_ENV") ?? "local"}.json")
+    .AddJsonFile($"ocelot.{env}.json")
     .AddEnvironmentVariables();
 
 builder.Services.AddOcelot();
